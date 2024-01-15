550be2fcd799b7953e258c6d27965dad7375f2f2
harfore <remiharfoush@gmail.com>
2024-01-15 11:18:26 +0100

    1.0

---
+++ Distracteur 1.0/Distracteur/resources.js
@@ -0,0 +1,61 @@
+// resources.js
+
+const resources = {
+  musiqueUrls: [
+    "https://youtu.be/4vJy61dkm60",
+    "https://youtu.be/hC8CH0Z3L54",
+    "https://youtu.be/Dv4Co7qpTnw",
+    "https://youtu.be/APWhx97QvxE",
+    "https://youtu.be/gdsUKphmB3Y",
+    "https://youtu.be/t6omUxqhG78",
+    "https://youtu.be/dWRCooFKk3c",
+    "https://youtu.be/Nuanwn3v-2I",
+    "https://youtu.be/fYEXdCCpfVQ",
+    "https://youtu.be/ovvn5h8VpBo",
+    "https://www.youtube.com/watch?v=rhTl_OyehF8",
+    "https://www.youtube.com/watch?v=fC06V0ZLGwg",
+    'https://www.youtube.com/watch?v=NF-kLy44Hls',
+    'https://www.youtube.com/watch?v=2fDzCWNS3ig'
+  ],
+  videoUrls: [
+    "https://youtu.be/BGYIXarTNbA",
+    "https://youtu.be/sJs_64OUpEs",
+    "https://youtu.be/MWHN6ojlVXI",
+    "https://youtu.be/ThwuT3_AG6w",
+    "https://youtu.be/NIH1iGtDvJY",
+    "https://youtu.be/h42QVfrUVFw",
+    "https://youtu.be/VbeM8Lf7s5A",
+    "https://youtu.be/oXAsvg_ZZzk",
+    "https://youtu.be/TyUA1OmXMXA",
+    "https://www.youtube.com/watch?v=cqGjhVJWtEg",
+    'https://www.youtube.com/watch?v=qh6K0z7-700&list=PLndQKPttFXMD4SZGqV84TOEQy5hffK5Nv&index=4'
+  ],
+  articlesUrls: [
+    "https://www.legorafi.fr/category/horoscope-2/",
+    "https://www.legorafi.fr/2024/01/08/la-societe-generale-va-ajouter-une-musique-de-suspense-lorsque-vous-consultez-vos-comptes-le-lundi-matin/",
+    "https://www.legorafi.fr/2024/01/09/son-appli-de-meditation-lui-envoie-75-notifications-par-jour-pour-lui-rappeler-detre-dans-linstant-present/",
+    "https://www.legorafi.fr/2023/12/19/penurie-de-medicaments-le-gouvernement-recommande-de-mettre-une-gousse-dail-sous-son-oreiller/",
+    "https://www.legorafi.fr/2024/01/09/brigitte-macron-assurera-la-regence-le-temps-que-gabriel-attal-soit-en-age-de-gouverner/",
+    "https://www.theonion.com/americans-explain-why-the-military-is-too-woke-1851150689",
+    "https://www.legorafi.fr/2024/01/11/gabriel-attal-surprend-tout-le-monde-en-arrivant-a-matignon-en-abaya/",
+    "https://www.legorafi.fr/2024/01/10/jo-2024-pour-donner-une-bonne-image-de-paris-anne-hidalgo-va-organiser-un-grand-toilettage-des-rats-de-la-capitale/",
+    "https://www.legorafi.fr/2023/12/21/noel-faut-il-mentir-a-ses-enfants-ou-leur-reveler-que-le-pere-noel-est-un-male-blanc-de-plus-de-50-ans/",
+    'https://www.criticalimprov.com/index.php/csieci/article/view/850/1411',
+    "https://www.liberties.eu/fr/stories/halden-the-world-s-most-humane-prison/11089"
+  ],
+  jeuxUrls: [
+    "https://www.crazygames.fr/jeu/desktop-tower-defense",
+    "https://www.crazygames.fr/jeu/smash-karts",
+    "https://www.crazygames.fr/jeu/goober-dash",
+    "https://www.crazygames.fr/jeu/goober-dash",
+    "https://www.crazygames.fr/jeu/helix-jump",
+    "https://www.crazygames.fr/jeu/ragdoll-archers",
+    "https://www.crazygames.fr/jeu/mr-racer---car-racing",
+    "https://www.crazygames.fr/jeu/bonkio",
+    "https://www.jesuisundev.com/across-multiverse/",
+    "https://www.16personalities.com/fr/test-de-personnalite",
+    "https://www.queeringthemap.com/"
+  ]
+};
+
+export default resources;

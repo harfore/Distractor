adf53145594764d98caec2eec1a4c9b259ba7229
julieATS <146748819+julieATS@users.noreply.github.com>
2024-01-16 14:53:51 +0100

    bouton anecdote ok

---
+++ Distracteur_version_julie/process.js
@@ -0,0 +1,13 @@
+/*
+click on button qui appelle
+changeMessageType('une video')) qui appelle
+changeMessage() qui
+    change texte menu
+    si currentWindow
+        efface ancienne fenêtre
+    displayRandomAvailableResources() ouvre nouvelle fenêtre qui appelle
+    getAvailableResources() et
+    markResourceAsDisplayed(randomUrl) et
+!!!    openWindow(randomUrl, height, width);
+    return randomUrl
+*/

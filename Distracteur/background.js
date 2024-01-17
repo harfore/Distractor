897ffcb5b92addbb354b27b24e50bb0218eaec4f
Stoyots <146749062+Stoyots@users.noreply.github.com>
2024-01-17 16:45:48 +0100

    final(ish) version
    dislike marche pas encore

---
+++ Distracteur/background.js
@@ -0,0 +1,43 @@
+chrome.runtime.onInstalled.addListener(function () {
+    console.log('Extension installée.');
+});
+
+chrome.runtime.onInstalled.addListener(function (details) {
+    if (details.reason === 'install' || details.reason === 'update') {
+
+        let currentPopupId = null;
+
+        // Fonction pour créer une nouvelle fenêtre popup
+        function createRandomPopup() {
+            if (currentPopupId !== null) {
+                chrome.windows.remove(currentPopupId)
+            }
+            const leftMargin = Math.floor(Math.random() * (1920 - 400 + 1)) + 400;
+            const topMargin = Math.floor(Math.random() * (1080 - 200 + 1)) + 200;
+            chrome.windows.create({
+                url: 'rappel.html', // Remplace par le chemin de ton popup HTML
+                type: 'popup',
+                width: 400,
+                height: 200,
+                left: leftMargin,
+                top: topMargin
+            }, function (window) {
+                currentPopupId = window.id
+            });
+        }
+
+        function getRandomInterval() {
+            return Math.floor((Math.random() * 60) + 40) * 1000
+            // (10 * 60 * 1000 - 1 * 60 * 1000 + 1) + 1 * 60 * 1000);
+        }
+        console.log(getRandomInterval())
+
+        // Appelle la fonction pour la première fois après le délai initial
+        setTimeout(createRandomPopup, getRandomInterval());
+
+        // Ensuite, répète l'appel à intervalles aléatoires entre 1 et 10 minutes
+        setInterval(function () {
+            createRandomPopup();
+        }, getRandomInterval());
+    }
+});
